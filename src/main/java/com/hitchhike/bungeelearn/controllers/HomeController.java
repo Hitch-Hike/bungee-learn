2ebd1e01e213f615f39ab101f348a575bc7572a8
johnnycashash <jagansiiinghchoudhary@gmail.com>
2018-03-18 01:05:30 +0530

    initial

---
+++ src/main/java/com/hitchhike/bungeelearn/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.hitchhike.bungeelearn.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/")
+    @ResponseBody
+    public String index(){
+        return "Hello World!";
+    }
+}
